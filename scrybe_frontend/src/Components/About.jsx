6312d1f4af694ba8e1912268d2bb1f75ae439226
Esteraj <estherajuzieogu@ESTHERs-MacBook-Pro.local>
2022-11-18 17:24:50 +0100

    feat:about-us page

---
+++ scrybe_frontend/src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function About() {
+  return (
+ <div className={styles.container}>
+    <div className={styles.row}>
+        <h1>
+        Scrybe helps businesses to make informed 
+        decisions <span>faster </span>
+        </h1>
+    </div>
+ </div>
+  )
+}
+
+export default About
